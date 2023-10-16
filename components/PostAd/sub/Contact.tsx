2a55e31a6ae08971d26372b6d15b080e5ef77e9f
muhib551 <muhib551@gmail.com>
2023-10-17 02:36:47 +0500

    created contact-info section

--- components/PostAd/sub/Contact.tsx
+++ components/PostAd/sub/Contact.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import LabelledInput from './LabelledInput';
+import Input from '@/components/Elements/Input';
+import Button from '@/components/Elements/Button';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact-wrapper">
+      <div className="ad-type-section-header">
+        <div className="post-ad-section-heading">
+          <span>CONTACT INFORMATION</span>
+        </div>
+      </div>
+      <div className="contact">
+        <div className="contact-info">
+          <LabelledInput labelText="Name" />
+          <LabelledInput labelText="Phone Number" />
+          <LabelledInput labelText="Email" />
+          <LabelledInput labelText="Location" />
+          <LabelledInput labelText="Grad" />
+          <LabelledInput labelText="Country" />
+        </div>
+        <div className="contact-button">
+          <Button
+            text="send an add"
+            style={{
+              textTransform: 'uppercase',
+              fontWeight: '700',
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- components/PostAd/sub/LabelledInput.tsx
+++ components/PostAd/sub/LabelledInput.tsx
@@ -1,3 +1,4 @@
+import Input from '@/components/Elements/Input';
 import React from 'react';
 
 interface Props {
@@ -5,7 +6,17 @@ interface Props {
 }
 
 const LabelledInput = ({ labelText }: Props) => {
-  return <div>{labelText}</div>;
+  return (
+    <div className="label-input">
+      <div className="label-text">{labelText}</div>
+      <Input
+        style={{
+          border: '0.5px solid #5E6762',
+          height: '56px',
+        }}
+      />
+    </div>
+  );
 };
 
 export default LabelledInput;
