0594d918f698bc0e8667887a787f5299a31c6024
muhib551 <muhib551@gmail.com>
2023-10-16 22:33:11 +0500

    added new css file

--- components/PostAd/sub/Header.tsx
+++ components/PostAd/sub/Header.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import '@/styles/postAd.css';
 
 const Header = () => {
-  return <div>Header</div>;
+  return <div className="">Header</div>;
 };
 
 export default Header;
