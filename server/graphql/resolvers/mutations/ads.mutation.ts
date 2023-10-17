c596cda8dfa4ef576a2e3995fc862b91cee2142c
nauroz <alinauroze@hotmail.com>
2023-10-17 23:26:19 +0500

    save changes

---
+++ server/graphql/resolvers/mutations/ads.mutation.ts
@@ -0,0 +1,74 @@
+import { IGqlContext } from '@/types';
+import type * as Prisma from '@prisma/client';
+import { isLoggedIn } from '../../wrappers';
+import prisma from '@/prisma/prisma';
+
+export const createAd = isLoggedIn(
+  async (
+    _: unknown,
+    {
+      price,
+      discountedPrice,
+      country,
+      city,
+      location,
+      details,
+      photos,
+    }: Prisma.Ad,
+    { user }: IGqlContext
+  ) => {
+    if (!details) {
+      throw new Error('Details are missing');
+    }
+
+    const ad = await prisma.ad.create({
+      data: {
+        submittedBy: user?.id as string,
+        country,
+        city,
+        location,
+        details,
+        price,
+        discountedPrice,
+        photos,
+      },
+    });
+
+    return ad;
+  }
+);
+
+export const updateAd = isLoggedIn(
+  async (
+    _: unknown,
+    {
+      id,
+      price,
+      discountedPrice,
+      country,
+      city,
+      location,
+      details,
+      photos,
+    }: Prisma.Ad
+  ) => {
+    if (details === null) {
+      throw new Error('Details can not be null');
+    }
+
+    const ad = await prisma.ad.update({
+      where: { id },
+      data: {
+        country,
+        city,
+        location,
+        details,
+        price,
+        discountedPrice,
+        photos,
+      },
+    });
+
+    return ad;
+  }
+);
