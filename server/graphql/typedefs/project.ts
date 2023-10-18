ee2913ecfe31d6e8fdfd61373f5600063ea7cfb1
nauroz <alinauroze@hotmail.com>
2023-10-18 15:09:33 +0500

    mutations

--- server/graphql/typedefs/project.ts
+++ server/graphql/typedefs/project.ts
@@ -10,6 +10,22 @@ const projectTypedefs = `
       fields: JSON
       createdOn: Date
     }
+
+    type Ad {
+      id: String
+      submittedBy: String
+      submittedByUser: User
+      isApproved: Boolean
+      title: String
+      price: String
+      discountedPrice: String
+      country: String
+      city: String
+      location: String
+      photos: [String]
+      details: JSON
+    }
+
     type Query {
       forms: [Form]
       users: [User]
@@ -27,6 +43,28 @@ const projectTypedefs = `
       deleteFieldForm (
         id: String!
       ): Form
+
+      createAd(
+        title: String
+        price: String
+        discountedPrice: String
+        country: String
+        city: String
+        location: String
+        photos: [String]
+        details: JSON
+      ): Ad
+      updateAd(
+        id: String!
+        title: String
+        price: String
+        discountedPrice: String
+        country: String
+        city: String
+        location: String
+        photos: [String]
+        details: JSON
+      ): Ad
     }
 `;
 
