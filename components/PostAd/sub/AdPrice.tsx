cc4f757c6454ef5e5b8f0d98ecc1e52db9bdd178
unknown <hassansattar147@gmail.com>
2023-10-18 15:25:54 +0500

    made ad desc as textarea

--- components/PostAd/sub/AdPrice.tsx
+++ components/PostAd/sub/AdPrice.tsx
@@ -1,12 +1,9 @@
 'use client';
-import React, { useState } from 'react';
-import DropDownMenu, { LV } from '@/components/Elements/Dropdown';
+import React from 'react';
+import DropDownMenu from '@/components/Elements/UCDropdown';
 import Input from '@/components/Elements/Input';
 
 const AdPrice = () => {
-  const [slectedPriceType, setSlectedPriceType] = useState<LV>();
-  const [slectedReplacement, setSlectedReplacement] = useState<LV>();
-
   return (
     <div className="ad-price-section-wrapper">
       <div className="ad-type-section-header">
@@ -26,42 +23,48 @@ const AdPrice = () => {
               }}
             />
           </div>
-          <div className="ad-price-section-pricing-type">
-            <DropDownMenu
-              options={[
-                { value: 'tempId1', label: 'Cash' },
-                { value: 'tempId1', label: 'Credit Card' },
-                { value: 'tempId1', label: 'Easypaisa' },
-                { value: 'tempId1', label: 'Raast' },
-              ]}
-              name="Price type"
-              selectedLV={slectedPriceType}
-              setSelectedLV={setSlectedPriceType}
+          <div className="price-input">
+            <Input
+              placeholder="Discount Price"
+              style={{
+                borderRadius: '15px',
+                border: 'none',
+                boxShadow: '0px 0 4px 2px #00000017',
+              }}
             />
           </div>
-          <div className="ad-price-section-pricing-type">
-            <DropDownMenu
-              options={[
-                { value: 'tempId2', label: 'Here is the replacement-1' },
-                { value: 'tempId2', label: 'Here is the replacement-2' },
-                { value: 'tempId2', label: 'Here is the replacement-3' },
-                { value: 'tempId2', label: 'Here is the replacement-4' },
-              ]}
-              name="Replacement"
-              selectedLV={slectedReplacement}
-              setSelectedLV={setSlectedReplacement}
-            />
-          </div>
+          {false && (
+            <>
+              <div className="ad-price-section-pricing-type">
+                <DropDownMenu
+                  options={[
+                    { value: 'tempId1', label: 'Cash' },
+                    { value: 'tempId1', label: 'Credit Card' },
+                    { value: 'tempId1', label: 'Easypaisa' },
+                    { value: 'tempId1', label: 'Raast' },
+                  ]}
+                  name="Price type"
+                />
+              </div>
+              <div className="ad-price-section-pricing-type">
+                <DropDownMenu
+                  options={[
+                    { value: 'tempId2', label: 'Here is the replacement-1' },
+                    { value: 'tempId2', label: 'Here is the replacement-2' },
+                    { value: 'tempId2', label: 'Here is the replacement-3' },
+                    { value: 'tempId2', label: 'Here is the replacement-4' },
+                  ]}
+                  name="Replacement"
+                />
+              </div>
+            </>
+          )}
         </div>
         <div className="price-description-detail">
           <p className="price-description-detail-heading">
             Ad Description <span className="steric">*</span>
           </p>
-          <p className="price-description-detail-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
-            laboriosam blanditiis deserunt amet voluptates dolorum odit,
-            expedita eum ipsa, reprehenderit ullam repellendus error placeat.
-          </p>
+          <textarea className="price-description-detail-text" />
         </div>
       </div>
     </div>
