3255c60c2ccbbaa4b6e931a2947846fb2110b394
nauroz <alinauroze@hotmail.com>
2023-10-18 15:38:13 +0500

    save changes

--- components/PostAd/index.tsx
+++ components/PostAd/index.tsx
@@ -11,18 +11,61 @@ import Footer from './sub/Footer';
 import Dynamic from './sub/Dynamic';
 
 import AdLocation from './sub/AdLocation';
-import { useQuery } from 'urql';
+import { gql, useMutation, useQuery } from 'urql';
 import { GET_FORMS, IForm } from '../AdminPanel/Forms';
 import Loading from '../Elements/Loading';
 import { useRouter, useSearchParams } from 'next/navigation';
 import fdtojson from '@/utils/fdtojson';
 import ChooseCateogry from './sub/ChooseCategory';
 
+const CREATE_AD = gql`
+  mutation CreateAd(
+    $title: String
+    $price: String
+    $discountedPrice: String
+    $country: String
+    $city: String
+    $location: String
+    $photos: [String]
+    $details: JSON
+  ) {
+    createAd(
+      title: $title
+      price: $price
+      discountedPrice: $discountedPrice
+      country: $country
+      city: $city
+      location: $location
+      photos: $photos
+      details: $details
+    ) {
+      city
+      id
+      country
+      details
+      discountedPrice
+      isApproved
+      location
+      photos
+      price
+      submittedBy
+      submittedByUser {
+        id
+        email
+        isAdmin
+        name
+      }
+      title
+    }
+  }
+`;
+
 const PostAd = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
   const [{ fetching, data: forms }] = useQuery({ query: GET_FORMS });
+  const [{ fetching: creating }, createAd] = useMutation(CREATE_AD);
 
   const form = forms?.forms?.find(
     (f: IForm) => f.category === searchParams.get('category')
@@ -34,6 +77,18 @@ const PostAd = () => {
     e.preventDefault();
     const fd = new FormData(e.target as HTMLFormElement);
     const json = fdtojson(fd);
+
+    createAd({
+      detail: json,
+
+      title: json.title,
+      price: parseInt(json.price) * 100,
+      discountedPrice: parseInt(json.discountedPrice) * 100,
+      country: json.country,
+      city: json.city,
+      location: json.location,
+      photos: json.photos.split('|'),
+    });
     console.log('JSON', json);
   };
 
