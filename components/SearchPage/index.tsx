e633a53055aeefdb9d6b6dd3d91a76edba36ceb4
muhib551 <muhib551@gmail.com>
2023-10-26 21:33:29 +0500

    message

--- components/SearchPage/index.tsx
+++ components/SearchPage/index.tsx
@@ -1,10 +1,25 @@
+'use client';
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../PostAd/sub/Footer';
 import HeroSection from './sub/HeroSection';
 import Card from './sub/Card';
 import NextPage from './sub/NextPage';
-import Filter from './sub/Filter';
+import '../../styles/PostAd.css';
+import FilterComp from './sub/Filter';
+import { DynamicFiltersResponse } from '@/types';
+
+const data: DynamicFiltersResponse[] = [
+  { type: 'checkbox', label: 'Airbags', section: 'Security' },
+  {
+    type: 'select',
+    label: 'Year',
+    section: 'Basic',
+    options: ['2001', '2002', '2003', '2204', '2005'],
+  },
+  { type: 'text', label: 'Other Info', section: 'Additional' },
+  { type: 'text', label: 'Power Info', section: 'Additional', addon: 'KM' },
+];
 
 const SearchPage = () => {
   return (
@@ -12,16 +27,16 @@ const SearchPage = () => {
       <Header />
       <div className="lg:flex lg:items-start lg:justify-between lg:h-max lg:mx-12">
         <div className="filter-section hidden lg:block w-1/2 ">
-          <Filter />
+          <FilterComp data={data} />
         </div>
         <div className="w-full">
           <HeroSection />
           <Card />
-          <Card />
+          {/* <Card /> */}
           <NextPage />
         </div>
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
--- components/SearchPage/sub/Filter.tsx
+++ components/SearchPage/sub/Filter.tsx
@@ -1,36 +1,89 @@
+import Input from '@/components/Elements/Input';
+import { DynamicFiltersResponse } from '@/types';
 import React from 'react';
 
-const data = [
-  { type: 'checkbox', label: 'Airbags', section: 'Security' },
-  {
-    type: 'select',
-    label: 'Year',
-    section: 'Basic',
-    options: ['2001', '2002', '2003', '2204', '2005'],
-  },
-  { type: 'text', label: 'Other Info', section: 'Additional' },
-  { type: 'text', label: 'Power Info', section: 'Additional', addon: 'KM' },
-];
+// const data = [
+//   { type: 'checkbox', label: 'Airbags', section: 'Security' },
+//   {
+//     type: 'select',
+//     label: 'Year',
+//     section: 'Basic',
+//     options: ['2001', '2002', '2003', '2204', '2005'],
+//   },
+//   { type: 'text', label: 'Other Info', section: 'Additional' },
+//   { type: 'text', label: 'Power Info', section: 'Additional', addon: 'KM' },
+// ];
 
-const Filter = () => {
+interface Props {
+  data: DynamicFiltersResponse[];
+}
+
+type ProcessedData = { [x: string]: DynamicFiltersResponse[] };
+
+const FilterComp = ({ data }: Props) => {
+  const [processedData, setProcessedData] = React.useState<ProcessedData>();
+
+  React.useEffect(() => {
+    const allSections = Array.from(new Set(data.map((e) => e.section)));
+    let temp: ProcessedData = {};
+
+    allSections.forEach((sectionTitle) => {
+      temp[sectionTitle] = data.filter((d) => d.section === sectionTitle);
+    });
+    setProcessedData({ ...temp });
+  }, [data]);
+
+  const sectionTitles = React.useMemo(() => {
+    return Object.keys(processedData || {});
+  }, [processedData]);
+
+  if (!processedData) return;
+
   return (
-    <div className="px-4 mt-6 bg-white">
-      <p className="py-2">Here will be Filter</p>
-      <p className="py-2">Here will be Filter</p>
-      <p className="py-2">Here will be Filter</p>
-      <p className="py-2">Here will be Filter</p>
-      <p className="py-2">Here will be Filter</p>
-      <p className="py-2">Here will be Filter</p>
-      <p className="py-2">Here will be Filter</p>
-      <p className="py-2">Here will be Filter</p>
-      <p className="py-2">Here will be Filter</p>
-      <p className="py-2">Here will be Filter</p>
-      <p className="py-2">Here will be Filter</p>
-      <p className="py-2">Here will be Filter</p>
-      <p className="py-2">Here will be Filter</p>
-      <p className="py-2">Here will be Filter</p>
+    <div className="mt-4">
+      {sectionTitles.map((sectionTitle, i) => {
+        const currentSectionElements = processedData[sectionTitle];
+        return (
+          <div key={i} className="">
+            <div className="">
+              <span>{sectionTitle}</span>
+            </div>
+
+            <div className={` bg-white `}>
+              {currentSectionElements.map((filterObj, elIndex) => {
+                if (filterObj.type === 'checkbox') {
+                  return (
+                    <div key={elIndex} className="">
+                      <input type="checkbox" /* name={el.name} */ />
+                      <label>{filterObj.label}</label>
+                    </div>
+                  );
+                } else if (filterObj.type === 'select') {
+                  return (
+                    <div key={elIndex}>
+                      <input type="checkbox" /* name={el.name} */ />
+                      <label htmlFor="">{filterObj.label}</label>
+                    </div>
+                  );
+                } else if (filterObj.type === 'text') {
+                  return (
+                    <div key={elIndex} className="">
+                      <p key={elIndex}>Min</p>
+                      <Input />
+                      <p key={elIndex}>Max</p>
+                      <Input />
+                      {filterObj.addon}
+                    </div>
+                  );
+                }
+                return null;
+              })}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
 
-export default Filter;
+export default FilterComp;
