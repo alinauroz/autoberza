afe7943015daea550ff5c39db7410197ebedd1b2
muhib551 <muhib551@gmail.com>
2023-10-31 01:22:18 +0500

    Updated MyAds page changes

--- components/MyAds/index.tsx
+++ components/MyAds/index.tsx
@@ -58,45 +58,53 @@ function MyAds() {
           </div>
         ) : (
           <div className="w-full  ">
-            {/* <MyAdCard /> */}
             {ads.map((ad: any) => (
               <div key={ad.id} className="mb-4">
                 <div className="bg-white p-4 flex gap-6">
-                  {/* {ad.photos[0]} */}
                   <Image
-                    src={ToyotaCar}
-                    alt=""
-                    className="lg:w-[500px] rounded-md w-[220px] 
-                    "
-                  />
-                  {/* <Image
                     src={ad.photos[0]}
                     alt=""
-                    width={}
-                    height={}
-                    className=""
-                  /> */}
-                  <div className="flex flex-col justify-between">
+                    width={236}
+                    height={130}
+                    className="rounded-md lg:w-[400px] h-[120] lg:h-[230px]"
+                  />
+                  {/* <div
+                    className="w-[406px] lg:w-[306px]"
+                    style={{
+                      backgroundImage: `url(${ad.photos?.[0]})`,
+                      backgroundSize: 'cover',
+                      backgroundPosition: 'center',
+                    }}
+                  ></div> */}
+                  <div className="flex flex-col justify-between w-full">
                     <div>
-                      <p className="text-lg font-bold">{ad.title}</p>
-                      <p className=" text-gray-600">
+                      <div className="flex justify-between">
+                        <p className="lg:text-lg font-bold text-md ">
+                          {ad.title}
+                        </p>
+                        <div className="flex items-center gap-2 hidden lg:flex">
+                          <Image src={Location} alt="" className="w-3" />
+                          <p className="text-xs font-semibold text-gray-600 lg:text-base">
+                            {ad.city}
+                          </p>
+                        </div>
+                      </div>
+                      <p className="text-gray-600 text-sm">
                         Created on:{' '}
                         {moment(new Date(ad.createdOn)).format('DD MMM YYYY')}
                       </p>
                     </div>
                     <div className="text-md font-semibold flex flex-col text-gray-500 gap-2">
-                      <p className="">{ad.city}</p>
-                      <p className="">{ad.country}</p>
-                      <p className="">{ad.location}</p>
+                      <p className="text-sm">{ad.category}</p>
                     </div>
-                    <div className="mb-1 ">
+                    <div className="mb-1 flex justify-between w-full">
                       <Link href={`/edit-ad?id=${ad.id}`}>
-                        <button className="bg-[#00C489] hover:bg-[#02b57f] active:bg-[#009669] px-8 py-2 mr-16 text-white text-sm font-semibold rounded-md">
+                        <button className="bg-[#00C489] hover:bg-[#02b57f] active:bg-[#009669] lg:px-8 lg:py-2 text-white lg:text-sm lg:font-semibold rounded-full text-xs px-4 py-1">
                           Edit
                         </button>
                       </Link>
                       <button
-                        className="bg-[#00C489] hover:bg-[#02b57f] active:bg-[#009669] px-8 py-2 mr-16 text-white text-sm font-semibold rounded-md"
+                        className="bg-red-600 hover:bg-red-700 active:bg-red-800 lg:px-8 lg:py-2 text-white lg:text-sm lg:font-semibold rounded-full text-xs px-4 py-1"
                         onClick={() => {
                           if (
                             window.confirm(
