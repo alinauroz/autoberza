6fc23aaa820906c672cec3b7ea6e44cc1b0f5871
muhib551 <muhib551@gmail.com>
2023-11-11 17:45:09 +0500

    miner changes

--- components/Ad/AdStat.tsx
+++ components/Ad/AdStat.tsx
@@ -2,24 +2,24 @@ import React from 'react';
 
 const MinMaxAvgComponent = ({ stats }: { stats: any }) => {
   return (
-    <div className="my-10 md:mt-10 md:my-0">
-      <div className="text-gray-800 flex md:flex-row md:items-center md:gap-16 gap-2 md:text-lg text-sm font-semibold md:w-full mx-auto">
-        <p className="font-bold">Stats:</p>
-        <div className="flex gap-2 w-full">
+    <div className="my-5 md:mt-10 md:my-0">
+      <div className="text-gray-800 flex md:flex-row md:items-center md:gap-16 gap-2 md:text-lg text-sm font-semibold md:w-full w-full mx-auto">
+        <p className="font-bold text-sm md:text-base">Stats:</p>
+        <div className="flex gap-1 w-full">
           <p>Min:</p>
           <div className="flex items-center bg-gray-200 px-2 rounded font-bold">
             {stats.min}
             <p>€</p>
           </div>
         </div>
-        <div className="flex gap-2 w-full">
+        <div className="flex gap-1 w-full">
           <p>Max:</p>
           <div className="flex items-center bg-gray-200 px-2 rounded font-bold">
             {stats.max}
             <p>€</p>
           </div>
         </div>
-        <div className="flex gap-2 w-full">
+        <div className="flex gap-1 w-full">
           <p>Avg:</p>
           <div className="flex items-center bg-gray-200 px-2 rounded font-bold">
             {stats.avg}
