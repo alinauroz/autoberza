c171a8579a610755dc1a09bae859d3dc3171f3fe
nauroz <alinauroze@hotmail.com>
2023-11-30 02:01:11 +0500

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,6 @@
-import Header from '@/components/Header/Header';
-import Footer from '@/components/PostAd/sub/Footer';
 import '../styles/postAd.css';
-import Link from 'next/link';
+import HomePage from '@/components/homepage';
 
 export default function Home() {
-  return (
-    <main>
-      <Header />
-      <div className="h-96">
-        <h1 className="AutoBerza Home"></h1>
-        <Link href="/search-page">Advance Search Ads here</Link>
-      </div>
-      <Footer />
-    </main>
-  );
+  return <HomePage />;
 }
