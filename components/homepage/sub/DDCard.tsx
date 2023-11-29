770d62f372a0bf3b7151dc51f1c4f361ff6ecd60
nauroz <alinauroze@hotmail.com>
2023-11-30 02:58:13 +0500

    fix: search

--- components/homepage/sub/DDCard.tsx
+++ components/homepage/sub/DDCard.tsx
@@ -3,6 +3,7 @@ import DropDown from './DropDown';
 import Button from '@/components/Elements/Button';
 import { gql, useQuery } from 'urql';
 import Link from 'next/link';
+import Loading from '@/components/Elements/Loading';
 
 const GET_FILTERS = gql`
   query Query {
@@ -18,17 +19,23 @@ const DDCard = () => {
   const filters = data?.adFilters?.filters || [];
 
   return (
-    <div className="bg-white rounded-md lg:w-[420px] xl:w-[500px] md:w-96 w-48 lg:rounded-xl py-2 md:py-6 lg:py-4 xl:py-12 xl:-ml-32">
-      <p className="text-[10px] md:text-2xl md:pb-3 lg:pb-4 xl:pb-5 lg:text-3xl xl:text-4xl font-semibold text-center ">
+    <div className="bg-white rounded-md lg:w-[550px] xl:w-[500px] md:w-96 w-[4/6] lg:rounded-xl py-2 md:py-6 lg:py-4 xl:py-12 xl:-ml-32">
+      <p className="my-6 md:text-2xl md:pb-3 lg:pb-4 xl:pb-5 lg:text-3xl xl:text-4xl font-semibold text-center ">
         Find your right car
       </p>
       <form action="/search">
-        <DropDown filters={filters} />
+        {fetching ? (
+          <div className="h-48 flex justify-center items-center">
+            <Loading />
+          </div>
+        ) : (
+          <DropDown filters={filters} />
+        )}
         <div className="flex items-center justify-between w-[80%] md:w-[90%] pt-[3px] md:pt-5 lg:w-full mx-auto ">
-          <p className="text-[9px] md:text-lg text-gray-500 font-semibold lg:hidden">
+          <p className="md:text-lg text-gray-500 font-semibold lg:hidden">
             <Link href="/search">Advance Search</Link>
           </p>
-          <button className="bg-[#00c489] hover:bg-[#02b07b] active:bg-[#10926b] w-max-content lg:w-full lg:mx-10 text-white font-semibold lg:font-bold rounded-[3px] md:rounded-md md:text-lg lg:text-xl py-[3px] lg:py-4 px-2 text-[8px]">
+          <button className="bg-[#00c489] hover:bg-[#02b07b] active:bg-[#10926b] w-max-content lg:w-full lg:mx-10 text-white font-semibold lg:font-bold rounded-[3px] md:rounded-md md:text-lg lg:text-xl py-[3px] lg:py-4 px-2">
             Search
           </button>
         </div>
--- components/homepage/sub/HeroSection.tsx
+++ components/homepage/sub/HeroSection.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 import Image from 'next/image';
 import homePageImg from '@/public/assets/common/homepage/homepage-img.svg';
 import DDCard from './DDCard';
+import useMobileDetect from '@/utils/useMobileDetect';
 
 const HeroSection = () => {
+  const isMobile = useMobileDetect();
+
   return (
     <div className="relative flex items-center justify-center w-full">
-      <Image src={homePageImg} alt="" className="w-full" />
-      <div className="absolute">
+      <Image
+        src={homePageImg}
+        alt=""
+        className="w-full absolute min-h-full"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="top right"
+      />
+      <div
+        className="bg-white relative z-40 my-10"
+        style={{
+          background: `url(${homePageImg?.src})`,
+          backgroundSize: 'cover',
+        }}
+      >
         <DDCard />
       </div>
     </div>
