edbc8a636f1a26c07b2de363c0be0f00cf245c19
nauroz <alinauroze@hotmail.com>
2023-11-30 13:07:47 +0500

    translations

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,20 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import { Provider } from 'urql';
 import { Toaster } from 'react-hot-toast';
+import { IntlProvider } from 'react-intl';
+import en from '../lang/en.json';
+import fr from '../lang/fr.json';
+import Cookies from 'js-cookie';
 
 const inter = Inter({ subsets: ['latin'] });
+const messages = { en, fr };
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const locale = (Cookies.get('locale') || 'fr') as 'fr' | 'en';
   return (
     <html lang="en">
       <head>
@@ -23,10 +29,12 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         {
-          <Provider value={client}>
-            {children}
-            <Toaster />
-          </Provider>
+          <IntlProvider locale={locale} messages={messages[locale]}>
+            <Provider value={client}>
+              {children}
+              <Toaster />
+            </Provider>
+          </IntlProvider>
         }
       </body>
     </html>
