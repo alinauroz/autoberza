fe17a8ec6482e14b4adf4b1295363b0a0e8e4832
nauroz <alinauroze@hotmail.com>
2023-12-05 16:49:52 +0500

    translation

--- components/PostAd/sub/Footer.tsx
+++ components/PostAd/sub/Footer.tsx
@@ -1,7 +1,7 @@
 'use client';
 import ButtonGroup from '@/components/Elements/ButtonGroup';
 import Dropdown from '@/public/assets/common/dropdown-icon.svg';
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import Image from 'next/image';
 import Facebook from '@/public/assets/common/facebok.svg';
 import Instagram from '@/public/assets/common/instagram.svg';
@@ -9,52 +9,7 @@ import Twitter from '@/public/assets/common/twitter.svg';
 import LogoImg from '@/public/assets/common/logo.svg';
 import FooterDropdown from '@/public/assets/common/footerDropdown.svg';
 import Link from 'next/link';
-import { FormattedMessage } from 'react-intl';
-
-const footerMenus = [
-  {
-    title: 'VEHICLE CATEGORIES',
-    items: [
-      { text: 'Automobiles', link: 'home-page' },
-      { text: 'Motorcycles and bicycles', link: 'home-page' },
-      { text: 'Transport vehicles', link: 'home-page' },
-      { text: 'Agricultural vehicles', link: 'home-page' },
-      { text: 'Working machines', link: 'home-page' },
-      { text: 'Nautical', link: 'home-page' },
-      { text: 'Other categories', link: 'home-page' },
-      { text: 'Parts and equipment', link: 'home-page' },
-      { text: 'Service and services', link: 'home-page' },
-      { text: 'Add an ad', link: 'post-ad?category=Bike#' },
-    ],
-  },
-  {
-    title: 'NAVIGATION',
-    items: [
-      { text: 'Shops', link: '#' },
-      { text: 'Auto parts', link: '#' },
-      { text: 'Car salons', link: '#' },
-      { text: 'Auto waste', link: '#' },
-      { text: 'Directory', link: '#' },
-      { text: 'Vehicle insurance', link: '#' },
-      { text: 'Auto news', link: '#' },
-      { text: 'SMS ads', link: '#' },
-      { text: 'Marketing', link: '#' },
-      { text: 'Site map', link: '#' },
-    ],
-  },
-  {
-    title: 'YOUR AUTODEALER',
-    items: [
-      { text: 'Mobile applications', link: '#' },
-      { text: 'Contact', link: 'contact-us' },
-      { text: 'Help', link: '#' },
-      { text: 'Terms of use', link: 'terms-conditions' },
-      { text: 'Privacy policy', link: 'privacy-policy' },
-      { text: 'Consumer rights', link: '#' },
-      { text: 'Safe trade', link: '#' },
-    ],
-  },
-];
+import { FormattedMessage, useIntl } from 'react-intl';
 
 const Footer = ({ containerClass }: { containerClass?: string }) => {
   const [isActive, setIsActive] = useState('');
@@ -62,6 +17,61 @@ const Footer = ({ containerClass }: { containerClass?: string }) => {
   const handleNavItems = (item: string) => {
     item === isActive ? setIsActive('') : setIsActive(item);
   };
+  const intl = useIntl();
+
+  const footerMenus = useMemo(
+    () => [
+      {
+        title: 'VEHICLE CATEGORIES',
+        items: [
+          { text: 'Automobiles', link: 'home-page' },
+          { text: 'Motorcycles and bicycles', link: 'home-page' },
+          { text: 'Transport vehicles', link: 'home-page' },
+          { text: 'Agricultural vehicles', link: 'home-page' },
+          { text: 'Working machines', link: 'home-page' },
+          { text: 'Nautical', link: 'home-page' },
+          { text: 'Other categories', link: 'home-page' },
+          { text: 'Parts and equipment', link: 'home-page' },
+          { text: 'Service and services', link: 'home-page' },
+          { text: 'Add an ad', link: 'post-ad?category=Bike#' },
+        ],
+      },
+      {
+        title: 'NAVIGATION',
+        items: [
+          { text: 'Shops', link: '#' },
+          { text: 'Auto parts', link: '#' },
+          { text: 'Car salons', link: '#' },
+          { text: 'Auto waste', link: '#' },
+          { text: 'Directory', link: '#' },
+          { text: 'Vehicle insurance', link: '#' },
+          { text: 'Auto news', link: '#' },
+          { text: 'SMS ads', link: '#' },
+          { text: 'Marketing', link: '#' },
+          { text: 'Site map', link: '#' },
+        ],
+      },
+      {
+        title: 'YOUR AUTODEALER',
+        items: [
+          {
+            text: intl.formatMessage({
+              defaultMessage: 'Mobile applications',
+              id: 'footer.mobile-application',
+            }),
+            link: '#',
+          },
+          { text: 'Contact', link: 'contact-us' },
+          { text: 'Help', link: '#' },
+          { text: 'Terms of use', link: 'terms-conditions' },
+          { text: 'Privacy policy', link: 'privacy-policy' },
+          { text: 'Consumer rights', link: '#' },
+          { text: 'Safe trade', link: '#' },
+        ],
+      },
+    ],
+    [intl]
+  );
 
   return (
     <div className={containerClass}>
@@ -109,7 +119,10 @@ const Footer = ({ containerClass }: { containerClass?: string }) => {
         <div className="footer-btn">
           <Link href="/post-ad">
             <ButtonGroup
-              text="Post an Ad"
+              text={intl.formatMessage({
+                defaultMessage: 'Post an Ad',
+                id: 'header.header-ad',
+              })}
               icon={Dropdown}
               style={{
                 fontSize: '22px',
