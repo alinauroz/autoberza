a1675855126dd3564bbc7ce4170f89510ae0a336
nauroz <alinauroze@hotmail.com>
2024-01-26 01:33:23 +0500

    fix

--- components/Elements/LoginOption.tsx
+++ components/Elements/LoginOption.tsx
@@ -23,23 +23,25 @@ const LoginOption = ({
   }, []);
 
   return (
-    <div className="grid lg:grid-cols-2 justify-between gap-2 py-5 w-full ">
-      <form action="/api/auth/signin/facebook" method="POST">
-        <input type="hidden" name="csrfToken" value={csrf} />
-        <input type="hidden" name="callbackUrl" value="/" />
-        <button
-          type="submit"
-          className="w-full flex items-center justify-center gap-5 border hover:bg-gray-200 active:bg-gray-300 border-gray-300 rounded-full py-4"
-        >
-          <Image src={fbIcon} alt="Facebook logo" className="w-[40px]" />
-          <p className="text-gray-500 font-bold md:text-lg">
-            <FormattedMessage
-              defaultMessage={facebookText}
-              id="loginsignupgooglefacebook.facebook"
-            />
-          </p>
-        </button>
-      </form>
+    <div className="lg:grid lg:grid-cols-2 justify-between gap-2 py-5 w-full ">
+      <div className="w-full my-2 lg:my-0">
+        <form action="/api/auth/signin/facebook" method="POST">
+          <input type="hidden" name="csrfToken" value={csrf} />
+          <input type="hidden" name="callbackUrl" value="/" />
+          <button
+            type="submit"
+            className="w-full flex items-center justify-center gap-5 border hover:bg-gray-200 active:bg-gray-300 border-gray-300 rounded-full py-4"
+          >
+            <Image src={fbIcon} alt="Facebook logo" className="w-[40px]" />
+            <p className="text-gray-500 font-bold md:text-lg">
+              <FormattedMessage
+                defaultMessage={facebookText}
+                id="loginsignupgooglefacebook.facebook"
+              />
+            </p>
+          </button>
+        </form>
+      </div>
       <form action="/api/auth/signin/google" method="POST">
         <input type="hidden" name="csrfToken" value={csrf} />
         <input type="hidden" name="callbackUrl" value="/" />
